180af9d7970bc73f1347af4ef7155b3434d8970b
muhammadsyafiq1 <syafiqmuhammad655@gmail.com>
2023-11-22 15:58:39 +0700

    masukan ke keranjang

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Swal from 'sweetalert2';
 import axios from 'axios';
 import {API_URL} from "./utils/const.js"
 import { Col, Container, Row } from "react-bootstrap";
 import { Hasil, ListCategories, Menu, NavbarComponent } from './Components';
 import React, { Component } from 'react'
+import Swal from 'sweetalert2'
 
 export default class App extends Component {
   constructor(props) {
@@ -11,7 +11,8 @@ export default class App extends Component {
   
     this.state = {
       menus: [],
-      pilihKategori : 'Makanan'
+      pilihKategori : 'Makanan',
+      keranjangs : []
     }
   }
   async componentDidMount() {
@@ -30,12 +31,34 @@ export default class App extends Component {
       pilihKategori : value,
       menus:[]  //bisa dihapus
     })
-
     axios
       .get(API_URL+"products?category.nama="+value)
       .then(res => {
         const menus = res.data;
         this.setState({menus});
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+  }
+  masukKeranjang = (value) => {
+    const keranjang = {
+      jumlah: 1,
+      total_harga: value.harga,
+      product: value
+    }
+    
+    axios
+      .post(API_URL+"keranjangs", keranjang)
+      .then(res => {
+        Swal.fire({
+          title: "Berhasil Ditambahkan",
+          text: "Berhasil Ditambahkan" +keranjang.product.nama,
+          imageUrl: "assets/images/"+keranjang.product.category.nama.toLowerCase()+"/"+keranjang.product.gambar,
+          imageWidth: 400,
+          imageHeight: 200,
+          imageAlt: "Custom image"
+        });
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -61,6 +84,7 @@ export default class App extends Component {
                   <Menu 
                     key={menu.id}
                     menu={menu}
+                    masukKeranjang={this.masukKeranjang}
                   />
                 ))}
                 </Row>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Card, Col } from 'react-bootstrap'
 import {numberWithCommas} from '../utils/numberFormat.js'
 
-const Menu = ({menu}) => {
+const Menu = ({menu, masukKeranjang}) => {
   return (
     <Col lg={6} xs={6} className="mb-3 mr-3">
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem' }} onClick={()=>masukKeranjang(menu)}>
             <Card.Img variant="top" src={"assets/images/"+menu.category.nama.toLowerCase()+"/"+menu.gambar} />
             <Card.Body>
             <Card.Title>{menu.nama}</Card.Title>
